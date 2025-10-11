1aecd85bc3a2cddbcc8c621c65bbf2b309a0f40d
olamiwale <olamiwalepaul@gmail.com>
2025-10-11 09:16:56 +0100

    teraform automation added

---
+++ terraform/aks.tf
@@ -0,0 +1,31 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "briitzaks"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  dns_prefix          = "briitzaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = true
+
+  network_profile {
+    network_plugin     = "azure"
+    load_balancer_sku  = "standard"
+  }
+} 
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,12 @@
+variable "subscription_id" {
+  type        = string
+  description = "8acdb728-9561-4071-afaa-cf8227e09d2f"
+}
+
+variable "resource_group_name" {
+  default = "briitzresourcegroup"
+}
+
+variable "location" {
+  default = "eastus"
+}
